fd6429826191bcc81879bf6d70b480750e05ffed
Amel guechi <151245703+amelcode@users.noreply.github.com>
2025-04-29 17:54:52 +0100

    Update 3_2_recent.sql

--- 3_2_recent.sql
+++ 3_2_recent.sql
@@ -1,1 +1,9 @@
+-- Display the name (NomLab) of the most recent created Laboratoire
+SELECT NomLab
+FROM Laboratoire
+WHERE Datcreation = (
+    SELECT MAX(Datcreation)
+    FROM Laboratoire
+);
+GO
 
